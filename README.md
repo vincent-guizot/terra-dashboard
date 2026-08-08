a291ec2ed2b42016dfd5f7426b87eb241de128b0
vincent-guizot <vincentguizot128@gmail.com>
2026-08-09 05:00:22 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Dashboard
+
+## Technology Stack
+
+A. Front End
+
+    1. Next.js
+    2. Tailwind
+    3. React Icons 
+
+B. Back En
+
+    1. Mongo 
+    2. Express.js
+    3. Strapi
---
+++ ui/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+// Components
+
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="container">
+       <Header/>
+    </div>
+  );
+}
+
+export default App;
